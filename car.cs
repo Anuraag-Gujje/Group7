a6d5ef307cc33417cb8d0149cdabcf5098852ed4
prudhvitejamamidi <mamidiprudhviteja96@gmail.com>
2024-03-05 15:59:05 -0500

    Test

---
+++ car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Group7
+{
+	public class car
+	{
+		public string CarModel;
+		public int CarNumber;
+	public string getCarModel()
+		{
+			return this.CarModel;
+		}
+	public int getCarNUmber()
+		{
+			return this.CarNumber;
+		}
+	}
+}
+
